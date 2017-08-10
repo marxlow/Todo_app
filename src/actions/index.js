724a42375ce42fb63e77ac8133abbe179a9b805e
marxlow <marx_low94@hotmail.com>
2017-08-10 14:11:31 -0400

    Added folders of Actions, Constants, Reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,14 @@
 import * as ACTION_TYPES from '../constants';
 
+let nextId = 0
+
 export const CreateTask = (taskname) => ({
   type: ACTION_TYPES.CREATE_TASK,
-  taskname,
+  id: nextId ++,
+  taskname
 });
+
+export const TOGGLE_TASK = (index) => ({
+  type: ACTION_TYPES.TOGGLE_TASK,
+  index
+})
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,28 @@
 import * as ACTION_TYPES from '../constants';
 
+const initialState = {
+  tasklist: {},
+  visibility: ACTION_TYPES.VISIBILITY_MODES.ALL
+}
 
-const reducer = function(state = [], action) {
+const reducer = function(state = initialState, action) {
   switch(action.type) {
     case ACTION_TYPES.CREATE_TASK:
-      return {
-        ...state
-      };
-
+      return [
+        ...state,
+        {
+          id: action.id,
+          text: action.taskname,
+          completed: false
+        },
+      ]
+    case ACTION_TYPES.TOGGLE_TASK:
+      return state.map(task =>
+        (task.id === action.id)
+        // If Id matches, change parameter
+        ? { ...task, completed: !task.completed }
+        : task
+      )
     default:
       return state;
   }
